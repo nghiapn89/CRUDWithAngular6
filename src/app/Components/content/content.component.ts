79a6ffe5e294df93034045e9832326e0446c3d92
Nghia <nghiapn89@gmail.com>
2018-10-17 22:53:49 +0700

    Commit lần đầu

---
+++ src/app/Components/content/content.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit, NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule} from '@angular/router';
+
+@Component({
+  selector: 'app-content',
+  templateUrl: './content.component.html',
+  styleUrls: ['./content.component.css']
+})
+export class ContentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,33 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { RouterModule} from '@angular/router';
+
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './Components/header/header.component';
+import { FooterComponent } from './Components/footer/footer.component';
+import { MenuComponent } from './Components/menu/menu.component';
+import { ContentComponent } from './Components/content/content.component';
+import { ProductComponent } from './Pages/product/product.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    MenuComponent,
+    ContentComponent,
+    ProductComponent,
+  ],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot([
+      {
+         path: 'app-product',
+         component: ProductComponent
+      }
+   ])
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
